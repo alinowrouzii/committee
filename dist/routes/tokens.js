6286aaa3130e4d9b238317297b1ba51a27e0c084
Ali Nowrouzi <alinowrouzii@gmail.com>
2022-05-08 04:27:03 -0400

    RM dist

--- dist/routes/tokens.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var express = require('express');
-var get_token = require('./../controllers/tokenController').get_token;
-var router = express.Router();
-// router.post('/token', createPost);
-module.exports = router;
