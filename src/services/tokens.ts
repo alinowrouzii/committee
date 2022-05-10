721a667a7fae020dff306fb4254c1cc95b3590f7
alinowrouzii <alinowrouzii@gmail.com>
2022-05-10 08:58:20 +0430

    Add refresh token api call

--- src/services/tokens.ts
+++ src/services/tokens.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 import config from "../config/index";
 import Token from "../models/Token";
-const { user_private_token } = config;
 import { client } from "./../config/axios";
+const { user_private_token } = config;
 
 const generate_initial_token = async () => {
-    console.log("shit3");
 
     const headers = {
         Authorization: `Basic ${user_private_token}`,
@@ -19,23 +18,56 @@ const generate_initial_token = async () => {
         scopes: `${config.scopes}`,
     });
 
-    const result = await client({}).post(
+    const result = await axios.post(
         "https://apibeta.finnotech.ir/dev/v2/oauth2/token", data, {
         headers: headers,
     }
     );
 
     console.log("res", result.data);
+    const token = new Token({
+        token: result.data.result.value,
+        refresh_token: result.data.result.refreshToken
+    });
 
+    await token.save();
 };
 
 const generate_new_token = async function () {
-    const token = await Token.find();
-    if (token.length === 0) {
-        console.log("here");
-        return generate_initial_token();
+    const tokens = await Token.find();
+    // if (tokens.length === 0) {
+    //     console.log("here");
+    //     await generate_initial_token();
+    //     return 
+    // }
+    const token = tokens[0];
+    console.log(token)
+    console.log("===========")
+    const refresh_token = token.refresh_token
+
+    const headers = {
+        Authorization: `Basic ${user_private_token}`,
+        "Content-Type": "application/json",
+    };
+    console.log(headers);
+
+    var data = JSON.stringify({
+        grant_type: "refresh_token",
+        refresh_token: refresh_token,
+        token_type: "CLIENT-CREDENTIAL",
+    });
+
+    const result = await axios.post(
+        "https://apibeta.finnotech.ir/dev/v2/oauth2/token", data, {
+        headers: headers,
     }
-    const refresh_token = token[0].refresh_token;
+    );
+
+    token.refesh_token = result.data.result.refreshToken
+    token.token = result.data.result.value
+
+    await token.save();
+    console.log(token)
 };
 
 export { generate_new_token, generate_initial_token };
